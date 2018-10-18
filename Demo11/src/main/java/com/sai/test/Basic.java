ab27a9ae43c571b6bbe7d64231dd527e58179d87
saikrishna.rathikanti <saikrishna.rathikanti@khumbusystems.com>
2018-10-18 05:58:24 +0000

    Updated Basic.java

--- Demo11/src/main/java/com/sai/test/Basic.java
+++ Demo11/src/main/java/com/sai/test/Basic.java
@@ -5,7 +5,7 @@ public class Basic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("i am in Khumbu....!");
+System.out.println("saikrishna here....!");
 
 	}
 
